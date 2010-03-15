cb4687254c30bc2af7307f6c84a7c8b9fc8e4fd5
Matt Scilipoti & Timothy King <pair+mms+tmk@pivotallabs.com>
2010-03-15 09:56:38 -0400

    Passes homepage feature.

--- config/environment.rb
+++ config/environment.rb
@@ -33,9 +33,14 @@ Rails::Initializer.run do |config|
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
-  config.time_zone = 'UTC'
+  #  config.time_zone = 'UTC'
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+
+  config.time_zone = 'UTC'
+
+  config.gem 'haml', :version => '2.2.21'
+  config.gem 'inherited_resources', :version => '1.0.3'
 end
--- features/homepage.feature
+++ features/homepage.feature
@@ -2,8 +2,7 @@ Feature: In order to present a welcome page to guests and ambassadors
   the homepage
   Should allow a new user to select Guest or Ambassador
 
-  @wip
   Scenario: new user comes to app
     When I go to the homepage
-    Then I should see "Guest of B'more"
-    And I should see "Ambassador for B'more"
+    Then I should see "B'more Guests"
+    And I should see "B'more Ambassadors"
